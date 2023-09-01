994b60477a640768c5c987c67c0de4fc061e8d96
adyashapattanaik19 <142867325+adyashapattanaik19@users.noreply.github.com>
2023-09-01 15:00:39 +0530

    Update rotatearrayby1.js

--- arrays/rotatearrayby1.js
+++ arrays/rotatearrayby1.js
@@ -12,36 +12,3 @@ function rotate(nums, k) {
    let nums= [1,2,3,45];
    let k=1;
    rotate(nums,k);
-
-// function Rotate_and_Print(arr,d,n)
-// {
-// 	var temp=new Array(n);
-	
-// 	let k = 0;
-
-	
-// 	for (let i = d; i < n; i++) {
-// 		temp[k] = arr[i];
-// 		k++;
-// 	}
-
-
-// 	for (let i = 0; i < d; i++) {
-// 		temp[k] = arr[i];
-// 		k++;
-// 	}
-	
-// 	for (let i = 0; i < n; i++) {
-// 		console.log(temp[i]+" ");
-// 	}
-// }
-
-// let arr = [ 1, 2, 3, 4, 5, 6, 7 ];
-// let n = arr.length;
-// let d = 1; //number of times rotating the array
-// Rotate_and_Print(arr, d, n);
-
-
-
-
-
