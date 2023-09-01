e91ba153aaa3d413ecc7bf02a67d208b9cd3b3bc
adyashapattanaik19 <142867325+adyashapattanaik19@users.noreply.github.com>
2023-09-01 15:02:08 +0530

    Update removeduplicates.js

--- strings/removeduplicates.js
+++ strings/removeduplicates.js
@@ -1,22 +1,11 @@
 function removeduplicates(){
 let arr=[5,3,5,5,3,4,6];
-let obj = Object.assign({},arr);
-//console.log(JSON.stringify(obj));
-
-let arr1 = [];
-let newobj={};
-	for( let i = 1 ;i < arr.length ;i++){
-        //arr1=arr[i];
-        // for(let j= 0; j<arr1.length; j++)
-        if(arr[i]=true){
-            break;
-        }else{
-            arr1.push(arr)
-        }
-    newobj[arr1]=arr[i];
+let obj={};
+	for( let i = 0 ;i < arr.length ;i++){
+        
+        obj[arr[i]]=true;
     }
-    console.log(arr1)  
+    console.log(Object.keys(obj));
 }
 removeduplicates();
-
 
