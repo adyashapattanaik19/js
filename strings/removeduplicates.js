ca76cf0d1625f59384227ea29abfb7aa50cf9454
adyashapattanaik19 <142867325+adyashapattanaik19@users.noreply.github.com>
2023-09-01 10:17:50 +0530

    Update removeduplicates.js

--- strings/removeduplicates.js
+++ strings/removeduplicates.js
@@ -1,16 +1,10 @@
 function removeduplicates(){
 let arr=[5,3,5,5,3,4,6];
-let arr1 = [];
 let obj={};
 	for( let i = 0 ;i < arr.length ;i++){
         
         obj[arr[i]]=true;
-        console.log(Object.keys(obj));
-
     }
+    console.log(Object.keys(obj));
 }
 removeduplicates();
-
-
-
-
